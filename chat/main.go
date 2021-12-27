f98711269ce8852e364353b8ce6db94283747063
hero <esistkalt@naver.com>
2021-12-27 21:24:48 +0100

    add basic web socket chat application

---
+++ chat/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		w.Write([]byte(`
+		<html>
+			<head> <title>Chat</title> </head>
+			<boody> Lets' Chat </body>
+		</html>
+		`))
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe : ", err)
+	}
+}
