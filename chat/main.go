f640309d0b179dfb28733c3ac9410e3d9270912c
hero <esistkalt@naver.com>
2021-12-29 22:16:48 +0100

    add tracer

--- chat/main.go
+++ chat/main.go
@@ -1,33 +1,42 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
+
+	"golang.org/x/net/trace"
 )
 
 type templateHandler struct {
-	once    sync.Once
-	filname string
-	templ   *template.Template
+	once     sync.Once
+	filename string
+	templ    *template.Template
 }
 
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
-		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filname)))
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 		t.templ.Execute(w, nil)
 	})
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application")
+	flag.Parse()
+
 	r := newRoom()
-	http.Handle("/", &templateHandler{filname: "chat.html"})
+	r.tracer = trace.New(os.Stdout)
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 
 	go r.run()
 
+	log.Println("Starrting web server on : ", *addr)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe : ", err)
 	}
